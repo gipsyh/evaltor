47cecfb290bd7c89e35ac2782e2388de84f3ab9c
Yuheng Su <gipsyh.icu@gmail.com>
2023-10-08 13:07:03 +0800

    update

--- utils/plot.py
+++ utils/plot.py
@@ -2,18 +2,19 @@ import matplotlib.pyplot as plt
 import sys
 
 if __name__ == "__main__":
-    for file in sys.argv[1:]:
+    for file_name in sys.argv[1:]:
         data = []
-        with open(file, "r") as file:
+        with open(file_name, "r") as file:
             for line in file:
                 model, time = line.strip().split()
                 if time != "Timeout" and time != "Failed":
                     data.append(float(time))
         data = sorted(data)
-        plt.plot(range(len(data)), data, marker="x")
+        plt.plot(range(len(data)), data, marker="x", label = file_name)
 
     plt.title("result")
     plt.xlabel("solved")
     plt.ylabel("time")
+    plt.legend()
     plt.show()
     plt.savefig("result.png")
