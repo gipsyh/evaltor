0adce7704e240e70fa5b057ac45bca8091de52a5
Yuheng Su <gipsyh.icu@gmail.com>
2023-11-06 20:10:17 +0800

    update

--- src/evaluatees/myic3.rs
+++ src/evaluatees/myic3.rs
@@ -18,21 +18,3 @@ impl Evaluatee for MyIc3 {
         command
     }
 }
-
-pub struct MyIc3Pl;
-
-impl Evaluatee for MyIc3Pl {
-    fn name(&self) -> String {
-        "myic3r0".to_string()
-    }
-
-    fn version(&self) -> String {
-        "pl".to_string()
-    }
-
-    fn evaluate(&self, path: &str) -> Command {
-        let mut command = Command::new("../ic3pl");
-        command.arg(path);
-        command
-    }
-}
